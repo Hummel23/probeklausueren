330a0cc83978547810211613a310a1eb1e69f4f7
Saba Kues <nigistsaba@hotmail.com>
2015-07-12 00:07:04 +0200

    mit_ConcurrentHashMap

--- Figures.java
+++ Figures.java
@@ -9,6 +9,7 @@ import java.awt.event.MouseMotionListener;
 import java.awt.geom.Ellipse2D;
 import java.util.*;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.swing.*;
 
@@ -19,9 +20,12 @@ public class Figures extends JFrame implements MouseListener, MouseMotionListene
 	JToggleButton btnCircle, btnSquare;
 	JButton btnDelete;
 	ButtonGroup btngroup = new ButtonGroup();
-	List<Shape> fig = Collections.synchronizedList(new ArrayList<>());
-	int size = 100, x, y;
-	
+	ConcurrentHashMap<Point, Boolean> fig = new ConcurrentHashMap<Point, Boolean>();
+	static int size = 100;
+	int indexShape, distanceX, distanceY;
+	Point p;
+	boolean dragged;
+
 	public Figures (){
 		super ("Kreise und Rechtecke");
 		contentPane = this.getContentPane();
@@ -46,93 +50,115 @@ public class Figures extends JFrame implements MouseListener, MouseMotionListene
 		protected void paintComponent (Graphics g){
 			super.paintComponent(g);
 			Graphics2D g2 = (Graphics2D)g;
-			for (Shape shape : fig) {
-				if(shape instanceof Rectangle){
+			for (Point key : fig.keySet()) {
+				if(fig.get(key)==true){
 					g2.setColor(Color.green);
+					g2.fillRect(key.x-(size/2), key.y-(size/2), size, size);
 				}else{
 					g2.setColor(Color.blue);
+					g2.fillOval(key.x-(size/2), key.y-(size/2), size, size);
 				}
-				g2.fill(shape);
 			}
 		}
 	}
 
-	JPanel initButtons(){
-		JPanel panel = new JPanel();
+		JPanel initButtons(){
+			JPanel panel = new JPanel();
 
-		btnCircle = new JToggleButton("Kreise");
-		btnCircle.setSelected(true);
-		btnSquare = new JToggleButton("Rechtecke");
-		btngroup.add(btnSquare);
-		btngroup.add(btnCircle);
+			btnCircle = new JToggleButton("Kreise");
+			btnCircle.setSelected(true);
+			btnSquare = new JToggleButton("Rechtecke");
+			btngroup.add(btnSquare);
+			btngroup.add(btnCircle);
 
-		btnDelete = new JButton("Löschen");
-		btnDelete.addActionListener(new ActionListener() {
+			btnDelete = new JButton("Löschen");
+			btnDelete.addActionListener(new ActionListener() {
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
+				@Override
+				public void actionPerformed(ActionEvent e) {
 
-				String []option = {"Ja", "Nein"};
-				int close = JOptionPane.showOptionDialog(Figures.this, "Willst du wirklich alles löschen?",
-						"Wirklich löschen?", JOptionPane.YES_NO_OPTION,
-						JOptionPane.WARNING_MESSAGE, null,option, option[1]);
-				switch(close){
-				case JOptionPane.YES_OPTION:
-					Figures.this.fig.clear();
-					Figures.this.repaint();
-					break;
+					String []option = {"Ja", "Nein"};
+					int close = JOptionPane.showOptionDialog(Figures.this, "Willst du wirklich alles löschen?",
+							"Wirklich löschen?", JOptionPane.YES_NO_OPTION,
+							JOptionPane.WARNING_MESSAGE, null,option, option[1]);
+					switch(close){
+					case JOptionPane.YES_OPTION:
+						Figures.this.fig.clear();
+						Figures.this.repaint();
+						break;
+					}
 				}
-			}
-		});
-		panel.add(btnCircle);
-		panel.add(btnSquare);
-		panel.add(btnDelete);
+			});
+			panel.add(btnCircle);
+			panel.add(btnSquare);
+			panel.add(btnDelete);
 
-		return panel;
+			return panel;
 
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent e) {
-
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-	@Override
-	public void mousePressed(MouseEvent e) {
-		this.x = e.getX();
-		this.y = e.getY();
-	
-	}
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		if(btnSquare.isSelected()){
-			fig.add(new Rectangle (x, y, this.size, this.size));
-		}else{
-			fig.add(new Ellipse2D.Double(x, y, this.size, this.size));
 		}
-		repaint();
-	}
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		
-	}
-	@Override
-	public void mouseMoved(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 
-	}
-	public static void main(String[] args) {
-		Figures f = new Figures();
+		@Override
+		public void mouseClicked(MouseEvent e) {
 
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+		@Override
+		public void mousePressed(MouseEvent e) {
+			p = e.getPoint();
+			//			for (Point key : keySet()) {
+			//				
+			//			}
+			//			for (Boolean boolean : fig) {
+			//				if (shape.contains(p)){
+			//					dragged = true;
+			//					indexShape = fig.indexOf(shape);
+			//					distanceX = x - shape.getBounds().x;
+			//					distanceY = y - shape.getBounds().y;
+			//
+			//				}
+			//			}
+
+		}
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			p = e.getPoint();
+			boolean rect;
+			//			if (dragged){
+			//				fig.get(indexShape).getBounds().setLocation(x-distanceX, y-distanceY);
+			//				dragged = false;
+			//			}else{
+			if(btnSquare.isSelected()){
+				rect = true;
+			}else{
+				rect = false;
+			}
+			fig.put(p, rect);
+			repaint();
+		}
+		@Override
+		public void mouseDragged(MouseEvent e) {
+
+			if (dragged){
+
+			}
+		}
+		@Override
+		public void mouseMoved(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+
+		}
+		public static void main(String[] args) {
+			Figures f = new Figures();
+
+		}
 	}
-}
